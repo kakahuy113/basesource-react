ffeb287783a6463c40cf4f25975c94178eb4e1d6
kakahuy113 <kakahuy113@gmail.com>
2020-10-19 16:42:11 +0700

    source

---
+++ src/views/layouts/footer/footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./footer.scss";
+const footer = () => {
+	return (
+		<div>
+			
+		</div>
+	)
+}
+
+export default footer
---
+++ src/views/layouts/header/header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "header.scss"
+const header = () => {
+	return (
+		<div>
+			
+		</div>
+	)
+}
+import "header.scss"
+export default header
